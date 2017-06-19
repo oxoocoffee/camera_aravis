64a90612dd560d8f1a580976db7d2d7e48ca64e7
Mike Hosmar <mhosmar@clearpathrobotics.com>
2017-06-19 15:04:24 -0400

    Added aditional path hints for aravis lib.

--- CMakeModules/FindAravis.cmake
+++ CMakeModules/FindAravis.cmake
@@ -3,11 +3,13 @@ INCLUDE(FindPackageHandleStandardArgs)
 FIND_PATH(ARAVIS_INCLUDE_PATH arv.h
   "$ENV{ARAVIS_INCLUDE_PATH}"
   /usr/local/include/aravis-0.4
+  /usr/include/aravis-0.4
 )
 
 FIND_LIBRARY(ARAVIS_LIBRARY aravis-0.4
   "$ENV{ARAVIS_LIBRARY}"
   /usr/local/lib
+  /usr/lib
 )
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(ARAVIS DEFAULT_MSG
