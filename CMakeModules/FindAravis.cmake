2bb046191363ca71fd7ba5c3944f28e0e875a674
Lincoln Lorenz <llorenz@dataspeedinc.com>
2017-10-24 13:11:00 -0400

    updated the real FindAravis.cmake to look for aravis-0.6 as well as aravis-0.4

--- CMakeModules/FindAravis.cmake
+++ CMakeModules/FindAravis.cmake
@@ -1,18 +1,32 @@
 INCLUDE(FindPackageHandleStandardArgs)
 
-FIND_PATH(ARAVIS_INCLUDE_PATH arv.h
-  "$ENV{ARAVIS_INCLUDE_PATH}"
-  /usr/local/include/aravis-0.4
-  /usr/include/aravis-0.4
+FIND_PATH(Aravis_INCLUDE_PATH 
+NAMES arv.h
+HINTS
+  "$ENV{Aravis_INCLUDE_PATH}"
+  /usr/local/include
+  /usr/include
+PATH_SUFFIXES
+  aravis-0.4
+  aravis-0.6
 )
 
-FIND_LIBRARY(ARAVIS_LIBRARY aravis-0.4
+FIND_LIBRARY(Aravis_LIBRARIES
+NAMES
+  libaravis-0.4
+  libaravis-0.6
+  aravis
+  aravis-0.4
+  aravis-0.6
+  libaravis
+
+HINTS
   "$ENV{ARAVIS_LIBRARY}"
   /usr/local/lib
   /usr/lib
 )
 
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(ARAVIS DEFAULT_MSG
-  ARAVIS_INCLUDE_PATH
-  ARAVIS_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Aravis DEFAULT_MSG
+  Aravis_INCLUDE_PATH
+  Aravis_LIBRARIES)
 
